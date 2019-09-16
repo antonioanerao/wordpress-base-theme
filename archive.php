f2c1de13df49425b743874ee81af1220355a1201
= <anerao.junior@gmail.com>
2019-09-16 16:41:50 -0500

    Adicionado arquivo archive.php para exibir categorias, posts por data etc

---
+++ archive.php
@@ -0,0 +1,44 @@
+<?php get_header(); ?>
+
+    <img class="img-fluid" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>"
+         width="<?php echo get_custom_header()->width; ?>" alt="" />
+
+    <div class="content-area">
+        <main>
+            <section class="middle-area">
+                <div class="container">
+                    <div class="row">
+
+                        <div class="news col-md-8">
+                            <?php
+                            the_archive_title('<h1>', '</h1>');
+                            the_archive_description();
+                            // Se houver algum post
+                            if( have_posts() ):
+                                // Enquanto houver posts, mostre-os pra gente
+                                while( have_posts() ): the_post();
+
+                                    ?>
+
+                                    <?php get_template_part( 'template-parts/content', 'archive' ); ?>
+
+                                <?php endwhile; ?>
+                                <div class="row">
+                                    <div class="pages text-left col-6"><?php previous_posts_link("<< Newer posts");?></div>
+                                    <div class="pages text-right col-6"><?php next_posts_link("Older posts"); ?></div>
+                                </div>
+                            <?php else: ?>
+
+
+                                <p>There's nothing yet to be displayed...</p>
+
+                            <?php endif; ?>
+
+                        </div>
+                        <?php get_sidebar( 'blog' ); ?>
+                    </div>
+                </div>
+            </section>
+        </main>
+    </div>
+<?php get_footer(); ?>
---
+++ template-parts/content-archive.php
@@ -0,0 +1,9 @@
+<article <?php post_class(); ?>>
+    <a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
+    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( array( 275, 275 ) ); ?></a>
+    <div class="meta-info">
+        <p>Published in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
+        <p><?php the_tags( 'Tags: ', ', ' ); ?></p>
+    </div>
+    <?php the_excerpt(); ?>
+</article>
